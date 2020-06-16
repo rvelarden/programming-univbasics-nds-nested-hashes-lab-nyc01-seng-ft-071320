48d910b45b58a6485b3c7d66a2f352ef271cb6ae
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-16 17:40:30 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,11 +21,13 @@ end
 
 def literal_aoh
   
-[{:name=>"Don Gately", :occupation=>"Live-in Staff"}
+[
+  {:name=>"Don Gately", :occupation=>"Live-in Staff"}
 {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
 {:name=>"Pat Monteseian", :occupation=>"Staff"}
 {:name=>"Kate Gompert", :occupation=>"None"}
-{:name=>"Bruce Green", :occupation=>"Fan of Mildred"}]
+{:name=>"Bruce Green", :occupation=>"Fan of Mildred"}
+]
 end
 
 def aoh_lookup(aoh, row, key)
