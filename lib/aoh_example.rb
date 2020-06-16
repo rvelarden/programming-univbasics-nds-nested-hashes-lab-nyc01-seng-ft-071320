dc1320f1f7aaaa76a9a00410222fb949edac1d32
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-16 17:39:52 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ end
 
 def literal_aoh
   
-{:name=>"Don Gately", :occupation=>"Live-in Staff"}
+hash = {:name=>"Don Gately", :occupation=>"Live-in Staff"}
 {:name=>"Joelle van Dyne", :occupation=>"Radio Personality"}
 {:name=>"Pat Monteseian", :occupation=>"Staff"}
 {:name=>"Kate Gompert", :occupation=>"None"}
