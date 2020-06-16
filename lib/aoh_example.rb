61322ffa51f21d06329fbd23d77ab5d75a78d7f4
Romina Velarde <rominapaola.velarde@gmail.com>
2020-06-16 17:31:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -20,8 +20,7 @@ def assembled_aoh
 end 
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
+  
 {DON_G.sort,JOELLE_VD.sort,PAT_M.sort,KATE_G.sort,BRUCE_G.sort}
 end
 
